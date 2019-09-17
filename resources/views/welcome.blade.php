ee093688d5e4a39c8be55fe9e1d1c342fc1dfeae
Kenny <carosiim.sdco@gmail.com>
2019-09-17 13:28:07 -0500

    navbar-side
    Aún faltan modificaciones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 <html>
 <head>
 	<title>Centro de Docencia Evaluaciones</title>
-    <!--Made with love by Mutiullah Samim -->
+    
     <link rel="icon" type="image/icon" href="{{ asset('/img/cdd.ico') }}" />	
 	<!--Bootsrap 4 CDN-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
